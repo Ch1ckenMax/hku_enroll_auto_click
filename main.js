a29aada75a3686fa30297b45a0c65e17e49daf10
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-07-13 14:32:18 +0800

    improved the variable scopes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function proceedStep2Of3(){ //Click function
-  var frame = document.getElementById("ptifrmtgtframe");
+  let frame = document.getElementById("ptifrmtgtframe");
   frame.contentDocument.getElementById("DERIVED_REGFRM1_LINK_ADD_ENRL$82$").click();
   frame.onload = function() { 
     frame.contentDocument.getElementById("DERIVED_REGFRM1_SSR_PB_SUBMIT").click();
@@ -9,13 +9,13 @@ var miliseclater;
 do{
   alert("Please input the click time. Make sure that the time is later than current time.");
   //Prompt the click time
-  var year = parseInt(prompt("Input Year"));
-  var month = parseInt(prompt("Input Month"));
-  var day = parseInt(prompt("Input Day"));
-  var hour = parseInt(prompt("Input hour"));
-  var minute = parseInt(prompt("Input minute"));
-  var timeNow = new Date(); //Retrieve current time
-  var clickTime = new Date(year,month - 1,day,hour,minute,0, 100); //Delay for 0.1 second as buffer
+  let year = parseInt(prompt("Input Year"));
+  let month = parseInt(prompt("Input Month"));
+  let day = parseInt(prompt("Input Day"));
+  let hour = parseInt(prompt("Input hour"));
+  let minute = parseInt(prompt("Input minute"));
+  let timeNow = new Date(); //Retrieve current time
+  let clickTime = new Date(year,month - 1,day,hour,minute,0, 100); //Delay for 0.1 second as buffer
   miliseclater = clickTime - timeNow;
 }while(miliseclater <= 0);
 var timer = setTimeout(proceedStep2Of3, miliseclater);

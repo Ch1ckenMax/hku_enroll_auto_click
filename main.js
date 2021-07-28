81a2512f1c041fd476c0ff812be9495caa08c3bc
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-07-28 13:51:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ do{
   let hour = parseInt(prompt("Input hour (24-hour format)"));
   let minute = parseInt(prompt("Input minute"));
   let timeNow = new Date(); //Retrieve current time
-  let clickTime = new Date(timeNow.getFullYear(),timeNow.getMonth(),timeNow.getDay(),hour,minute,0, 100); //Delay for 0.1 second as buffer
+  let clickTime = new Date(timeNow.getFullYear(),timeNow.getMonth(),timeNow.getDate(),hour,minute,0, 100); //Delay for 0.1 second as buffer
   miliseclater = clickTime - timeNow;
 }while(miliseclater <= 0);
 var timer = setTimeout(proceedStep2Of3, miliseclater);

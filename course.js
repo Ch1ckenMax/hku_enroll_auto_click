7caa7299847d542e6fb45626154d92d975b4c649
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-07-28 16:33:08 +0800

    Update course.js

--- course.js
+++ course.js
@@ -6,14 +6,29 @@ function proceedStep2Of3(){ //Click function
   }
 }
 var miliseclater = 0;
-do{
+while(miliseclater <= 0){
   alert("Please input the click time. Make sure that the time is later than current time.");
   //Prompt the click time
-  let hour = parseInt(prompt("Input hour (24-hour format)"));
-  let minute = parseInt(prompt("Input minute"));
+  let hour = prompt("Input hour (24-hour format)");
+  let minute = prompt("Input minute");
+  
+  //Validation
+  if(isNaN(hour) || isNaN(minute)){
+    alert("invalid input");
+    continue;
+  }
+
+  hour = parseInt(hour);
+  minute = parseInt(minute);
+  
+  if( (hour < 0 || hour >= 24) || (minute < 0 || minute >= 60) ){
+    alert("invalid time");
+    continue;
+  }
+  
   let timeNow = new Date(); //Retrieve current time
   let clickTime = new Date(timeNow.getFullYear(),timeNow.getMonth(),timeNow.getDate(),hour,minute,0, 100); //Delay for 0.1 second as buffer
   miliseclater = clickTime - timeNow;
-}while(miliseclater <= 0);
+}
 var timer = setTimeout(proceedStep2Of3, miliseclater);
 

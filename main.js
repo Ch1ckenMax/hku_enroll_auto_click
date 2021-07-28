2611f08a29f6d673dd7f571d2ba886360ce3d426
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-07-28 13:48:51 +0800

    remove prompt year month and day

--- main.js
+++ main.js
@@ -9,13 +9,10 @@ var miliseclater;
 do{
   alert("Please input the click time. Make sure that the time is later than current time.");
   //Prompt the click time
-  let year = parseInt(prompt("Input Year"));
-  let month = parseInt(prompt("Input Month"));
-  let day = parseInt(prompt("Input Day"));
   let hour = parseInt(prompt("Input hour (24-hour format)"));
   let minute = parseInt(prompt("Input minute"));
   let timeNow = new Date(); //Retrieve current time
-  let clickTime = new Date(year,month - 1,day,hour,minute,0, 100); //Delay for 0.1 second as buffer
+  let clickTime = new Date(timeNow.getFullYear(),timeNow.getMonth(),timeNow.getDay(),hour,minute,0, 100); //Delay for 0.1 second as buffer
   miliseclater = clickTime - timeNow;
 }while(miliseclater <= 0);
 var timer = setTimeout(proceedStep2Of3, miliseclater);

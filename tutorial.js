030d61ba073d7475f50760ad182d08eacf7d4b10
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-08-30 22:02:41 +0800

    Update and rename tutorialAbandoned.js to tutorial.js

---
+++ tutorial.js
@@ -0,0 +1,79 @@
+    window.addEventListener("load",() => {
+        //The button on the webpage cannot be clicked until the specified time. Therefore, the page has to be reloaded
+        //However, when the page is reloaded the chrome console clears all the javascript code
+        //To keep the code running, a status variable and the class to be clicked will be saved in Tampermonkey storage
+        //And this status varaible will determine which part of the code should be run
+        //Specifically, when the variable is true, the browser clicks the button
+        //otherwise(false/null), the browser asks for the input
+        if(GM_getValue("status",false)){
+            let classNum = GM_getValue("classNum",false);
+            if(classNum){
+                //The following commented section is for testing purpose
+                //document.getElementById(classNum).removeAttribute("disabled");
+                //document.getElementById(classNum).onclick = ()=>{alert(classNum)};
+              
+                document.getElementById(classNum).click();
+            }
+            else{
+                alert("error. cant find class variable");
+            }
+            GM_deleteValue("status");
+            GM_deleteValue("class");
+        }
+        else{
+            var miliseclater = 0, classNumber = -1;
+
+            //Prompt for class num, validate format, and verify the existence of the class
+            do{
+                let temp = prompt("Input class Number (3-digit format)");
+
+                //Validate format (is number, string length 3)
+                if(temp.length != 3 || isNaN(temp)){
+                    alert("Wrong format")
+                    continue;
+                }
+
+                //Remove zeros at start of string
+                if(temp[0] == '0'){
+                    temp = temp.substr(1);}
+                if(temp[0] == '0'){
+                    temp = temp.substr(1);}
+
+            //verify existence
+            if(document.getElementById("choice"+ (parseInt(temp) - 1).toString()) != null){
+                classNumber = parseInt(temp) - 1;}
+            else{
+                alert("Class does not exist");}
+
+            }while(classNumber == -1); //-1 represents wrong input
+
+            while(miliseclater <= 0){
+                alert("Please input the click time. Make sure that the time is later than current time.");
+                //Prompt the click time
+                let hour = prompt("Input hour (24-hour format)");
+                let minute = prompt("Input minute");
+
+                //Validation
+                if(hour.length == 0 || minute.length == 0 || isNaN(hour) || isNaN(minute)){
+                    alert("invalid input");
+                    continue;
+                }
+
+                hour = parseInt(hour);
+                minute = parseInt(minute);
+
+                if( (hour < 0 || hour >= 24) || (minute < 0 || minute >= 60) ){
+                    alert("invalid time");
+                    continue;
+                }
+
+                let timeNow = new Date(); //Retrieve current time
+                let clickTime = new Date(timeNow.getFullYear(),timeNow.getMonth(),timeNow.getDate(),hour,minute,0, 50); //Delay for 0.05 second as buffer
+                miliseclater = clickTime - timeNow;
+            }
+
+            var timer = setTimeout( () => {GM_setValue("status",true); GM_setValue("classNum","choice"+classNumber.toString()); window.location.reload(); } , miliseclater);
+        }
+
+
+    });

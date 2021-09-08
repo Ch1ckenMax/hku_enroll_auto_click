9f880bbdd913f4a3aa1298e04484c57832ca673c
Ch1ckenMax <59310578+Ch1ckenMax@users.noreply.github.com>
2021-09-08 14:24:31 +0800

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -13,7 +13,7 @@
                 //document.getElementById(classNum).onclick = ()=>{alert(classNum)};
               
                 document.getElementById(classNum).click();
-                document.getElementById("confirmToSubmit").click(); //Dialog box containing this button appears after the above click action. Need async? UNTESTED
+                document.getElementById("confirmToSubmit").click(); //Dialog box containing this button appears after the above click action.
                 
             }
             else{
